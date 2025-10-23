1f3ed244e6359772ec84a5510d9652d23c5220f6
nyrta1.exe <147030027+nyrta1@users.noreply.github.com>
2025-10-23 11:01:34 +0500

    Add current vehicles at gas station data

---
+++ currentVehiclesAtGasStation.js
@@ -0,0 +1,142 @@
+[
+  {
+    "id": 1,
+    "garageNumber": "A101",
+    "plateNumber": "547 BW 01",
+    "driver": "Калдыбеков Ерлан Нурланулы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 2,
+    "garageNumber": "A102",
+    "plateNumber": "592 AD 02",
+    "driver": "Серикбаев Даурен Алпысбайулы",
+    "vehicle": "Yutong ZK6126HGA - 12 м"
+  },
+  {
+    "id": 3,
+    "garageNumber": "A103",
+    "plateNumber": "015 ZZ 16",
+    "driver": "Парадабеков Нурлан Мырзакозауылы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 4,
+    "garageNumber": "A104",
+    "plateNumber": "777 BA 01",
+    "driver": "Ахметов Рустем Ержанулы",
+    "vehicle": "LiAZ 5292 - 12 м"
+  },
+  {
+    "id": 5,
+    "garageNumber": "A105",
+    "plateNumber": "311 BP 02",
+    "driver": "Мухамедов Азамат Сейдахметулы",
+    "vehicle": "MAN Lion’s City - 10 м"
+  },
+  {
+    "id": 6,
+    "garageNumber": "A106",
+    "plateNumber": "821 BK 01",
+    "driver": "Жуматаев Самат Берикович",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 7,
+    "garageNumber": "A107",
+    "plateNumber": "482 BX 01",
+    "driver": "Сулейменов Бахытжан Аскарович",
+    "vehicle": "Yutong ZK6119HA - 11 м"
+  },
+  {
+    "id": 8,
+    "garageNumber": "A108",
+    "plateNumber": "904 AB 02",
+    "driver": "Ержанов Дамир Муратулы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 9,
+    "garageNumber": "A109",
+    "plateNumber": "235 BN 16",
+    "driver": "Тулегенов Ермек Талгатович",
+    "vehicle": "Yutong ZK6126HGA - 12 м"
+  },
+  {
+    "id": 10,
+    "garageNumber": "A110",
+    "plateNumber": "991 BW 01",
+    "driver": "Парадабеков Нурлан Мырзакозауылы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 11,
+    "garageNumber": "A111",
+    "plateNumber": "662 CB 02",
+    "driver": "Калиев Ербол Сержанович",
+    "vehicle": "MAN Lion’s City - 10 м"
+  },
+  {
+    "id": 12,
+    "garageNumber": "A112",
+    "plateNumber": "533 AC 01",
+    "driver": "Султанов Айдын Сапарович",
+    "vehicle": "LiAZ 5292 - 12 м"
+  },
+  {
+    "id": 13,
+    "garageNumber": "A113",
+    "plateNumber": "407 BV 16",
+    "driver": "Исаев Нуржан Ерланулы",
+    "vehicle": "Yutong ZK6119HA - 11 м"
+  },
+  {
+    "id": 14,
+    "garageNumber": "A114",
+    "plateNumber": "218 BP 02",
+    "driver": "Карабаев Рахат Маратулы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 15,
+    "garageNumber": "A115",
+    "plateNumber": "359 AA 16",
+    "driver": "Калдыбеков Ерлан Нурланулы",
+    "vehicle": "LiAZ 5292 - 12 м"
+  },
+  {
+    "id": 16,
+    "garageNumber": "A116",
+    "plateNumber": "923 BN 01",
+    "driver": "Ахметов Рустем Ержанулы",
+    "vehicle": "Yutong ZK6126HGA - 12 м"
+  },
+  {
+    "id": 17,
+    "garageNumber": "A117",
+    "plateNumber": "605 BX 02",
+    "driver": "Жуматаев Самат Берикович",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  },
+  {
+    "id": 18,
+    "garageNumber": "A118",
+    "plateNumber": "184 BB 16",
+    "driver": "Парадабеков Нурлан Мырзакозауылы",
+    "vehicle": "LiAZ 5292 - 12 м"
+  },
+  {
+    "id": 19,
+    "garageNumber": "A119",
+    "plateNumber": "978 AD 01",
+    "driver": "Тулегенов Ермек Талгатович",
+    "vehicle": "Yutong ZK6119HA - 11 м"
+  },
+  {
+    "id": 20,
+    "garageNumber": "A120",
+    "plateNumber": "426 BW 02",
+    "driver": "Калдыбеков Ерлан Нурланулы",
+    "vehicle": "Iveco Urbanway PU - 12 м"
+  }
+]
