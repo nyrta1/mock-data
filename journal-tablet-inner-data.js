e02a15c732ab19bbdd6615f9b9e787419ee9e5e6
nyrta1.exe <147030027+nyrta1@users.noreply.github.com>
2025-11-04 01:58:53 +0500

    Create journal-tablet-inner-data.js with initial data
    Added initial data for journal tablet with work types and spare parts.

---
+++ journal-tablet-inner-data.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 1,
+    "workType": "Замена колодок",
+    "sparePart": "Колодка тормозная",
+    "quantity": 2,
+    "comment": "Передняя ось"
+  },
+  {
+    "id": 2,
+    "workType": "Проверка давления",
+    "sparePart": "-",
+    "quantity": "-",
+    "comment": "-"
+  },
+  {
+    "id": 3,
+    "workType": "Замена фильтра",
+    "sparePart": "Фильтр воздушный",
+    "quantity": 1,
+    "comment": "-"
+  }
+]
